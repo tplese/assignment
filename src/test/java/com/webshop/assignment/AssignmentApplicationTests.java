45d6c34c64d6704c5c0bdbc61944cccc8150736f
tplese <tplese@gmail.com>
2022-02-08 21:14:17 +0100

    added assertions to contextLoads test

--- src/test/java/com/webshop/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/webshop/assignment/AssignmentApplicationTests.java
@@ -1,14 +1,51 @@
 package com.webshop.assignment;
 
+import com.webshop.assignment.controller.CustomerController;
+import com.webshop.assignment.controller.OrderController;
+import com.webshop.assignment.controller.ProductController;
+import com.webshop.assignment.model.Customer;
+import com.webshop.assignment.service.CustomerService;
+import com.webshop.assignment.service.OrderResponseService;
+import com.webshop.assignment.service.OrderService;
+import com.webshop.assignment.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AssignmentApplicationTests {
 
-    @Test
-    void contextLoads() {
-        System.out.println("Test");
-    }
+    @Autowired
+    private OrderController orderController;
 
+    @Autowired
+    private CustomerController customerController;
+
+    @Autowired
+    private ProductController productController;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderResponseService orderResponseService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(orderController).isNotNull();
+        assertThat(customerController).isNotNull();
+        assertThat(productController).isNotNull();
+        assertThat(orderService).isNotNull();
+        assertThat(orderResponseService).isNotNull();
+        assertThat(customerService).isNotNull();
+        assertThat(productService).isNotNull();
+    }
 }
